cff94c6c3446ba07a89bc546ce847132da792405
Sean Seago <seagosean@gmail.com>
2020-03-03 20:23:24 -0600

    Milestone E21B03.1

---
+++ andromedadb-alt.py
@@ -0,0 +1,75 @@
+import datetime
+import hashlib
+import json
+from random import SystemRandom
+import uuid
+import zlib
+
+
+class AndromedaDB:
+    nonce = 99999999999
+
+    def gen_nonce():
+        return 1
+
+    class Document:
+        serial = 'doc_' + str(uuid.uuidv4())
+
+        def __init__(self):
+            pass
+
+        def insert(self, serial, title, data):
+            doc_hash = hashlib.sha256(data).digest()
+            doc_hexhash = hashlib.sha256(data).hexdigest()
+            doc_data = {'serial':serial, 'title': title, 'data':data, 'hash': doc_hash, 'nonce': gen_nonce}
+
+        def extract( self):
+            TODO: accept either title or serial as input to exctact a document
+
+    class Table:
+        serial = 'tab_' + str(uuid.uuidv4())
+
+        def __init__(self):
+            pass
+
+    class Edge:
+        serial = 'edg_' + str(uuid.uuidv4())
+
+        def __init__(self):
+            pass
+
+    class Vertex:
+        serial = 'ver_' + str(uuid.uuidv4())
+
+        def __init__(self):
+            pass
+
+    class Collection:
+        serial = 'col_' + str(uuid.uuidv4())
+
+        def __init__(self):
+            pass
+
+    class Assimilation:
+        serial = 'ass_' + str(uuid.uuidv4())
+
+        def __init__(self):
+            pass
+
+    class Hyperlink:
+        serial = 'hyp_' + str(uuid.uuidv4())
+
+        def __init__(self):
+            pass
+
+    class Block:
+        serial = 'blo_' + str(uuid.uuidv4())
+
+        def __init__(self):
+            pass
+
+    class Chain:
+        serial = 'cha_' + str(uuid.uuidv4())
+
+        def __init__(self):
+            pass
