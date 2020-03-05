d73a636cd47eaac414f7f205937331a1f72de450
Sean Seago <sean.seago@projectceladon.org>
2020-03-05 15:46:03 -0600

    Cleaned up, made results prettier

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ print(">> BEGINNING TEST -----------------------------------------------------")
 crypto_thing = SystemRandom()
 #nonce = crypto_thing.randrange(17179869184)
 blah_doc = {'module': 'pcas_core','mdep': None, 'odep':'python3, pip3, django' ,'tags':'test,red,99,bogus'}
-print(blah_doc)
+print('SAMPLE: ', blah_doc)
 
-print(">> INSERTING RECORD ---------------------------------------------------")
+
+print("\r\n\r\n>> INSERTING RECORD ---------------------------------------------------")
 doc_vitals = testDoc.insert("pcas_test", blah_doc)
 print('RESULT: ', doc_vitals)
 #print("doc vitals: ", doc_vitals)
 #print("serial: ", doc_vitals["serial"])
 
-print(">> EXTRACTING RECORD --------------------------------------------------")
+print("\r\n\r\n>> EXTRACTING RECORD --------------------------------------------------")
 doc_read = testDoc.extract(doc_vitals["serial"])
 print('RESULT: ', doc_read)
