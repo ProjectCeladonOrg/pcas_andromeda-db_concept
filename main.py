c70f366c88c104932cb92275445c1186ba088c4a
Sean Seago <sean.seago@projectceladon.org>
2020-03-06 09:13:46 -0600

    Cleaning, beautification

--- main.py
+++ main.py
@@ -10,17 +10,18 @@ testDoc = andromedadb.AndromedaDB.Document()
 
 print(">> BEGINNING TEST -----------------------------------------------------")
 crypto_thing = SystemRandom()
-#nonce = crypto_thing.randrange(17179869184)
-blah_doc = {'module': 'pcas_core','mdep': None, 'odep':'python3, pip3, django' ,'tags':'test,red,99,bogus'}
+blah_doc = {
+    'module': 'pcas_core',
+    'mdep': None,
+    'odep':'python3, pip3, django',
+    'tags':'test,red,99,bogus'
+}
 print('SAMPLE: ', blah_doc)
 
-
-print("\r\n\r\n>> INSERTING RECORD ---------------------------------------------------")
+print("\r\n\r\n>> INSERTING RECORD -------------------------------------------")
 doc_vitals = testDoc.insert("pcas_test", blah_doc)
 print('RESULT: ', doc_vitals)
-#print("doc vitals: ", doc_vitals)
-#print("serial: ", doc_vitals["serial"])
 
-print("\r\n\r\n>> EXTRACTING RECORD --------------------------------------------------")
+print("\r\n\r\n>> EXTRACTING RECORD ------------------------------------------")
 doc_read = testDoc.extract(doc_vitals["serial"])
 print('RESULT: ', doc_read)
